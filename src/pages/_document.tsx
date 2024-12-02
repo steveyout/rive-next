c488f45505df849ef67d4361b532f2a1988002ba
Steve Yout <34503856+steveyout@users.noreply.github.com>
2024-12-02 13:04:34 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,11 +23,21 @@ export default function Document() {
             gtag('config', 'G-MQN0EPT9M8');
             `}
           </Script>
+	      {/*
          <Script data-cfasync="false" strategy="afterInteractive">
             {`
 
            (function(){var i=window,p="ff04bed514e0b65a7e438197e1137c95",a=[["siteId",158+668+987+5108671],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],z=["d3d3LmJsb2NrYWRzbm90LmNvbS9CL2JjdGEubWluLmpz","ZG5oZmk1bm4yZHQ2Ny5jbG91ZGZyb250Lm5ldC9FWGlhL3B5YnRNRC9oZHJhd2VyLm1pbi5jc3M=","d3d3LnRvemJvaGpjdHdyaS5jb20vR29JL2JjdGEubWluLmpz","d3d3LmlrdnBsenRyZnEuY29tL3ZpdGsvWC9hZHJhd2VyLm1pbi5jc3M="],c=-1,q,l,h=function(){clearTimeout(l);c++;if(z[c]&&!(1749028238000<(new Date).getTime()&&1<c)){q=i.document.createElement("script");q.type="text/javascript";q.async=!0;var n=i.document.getElementsByTagName("script")[0];q.src="https://"+atob(z[c]);q.crossOrigin="anonymous";q.onerror=h;q.onload=function(){clearTimeout(l);i[p.slice(0,16)+p.slice(0,16)]||h()};l=setTimeout(h,5E3);n.parentNode.insertBefore(q,n)}};if(!i[p]){try{Object.freeze(i[p]=a)}catch(e){}h()}})();
             `}
+          </Script>
+	  */}
+	      <Script async strategy="afterInteractive" src="//acscdn.com/script/aclib.js"></Script>
+           <Script data-cfasync="false" strategy="lazyOnload">
+            {`
+            aclib.runPop({
+        zoneId: '9033646',
+     });
+             `}
           </Script>
 
 
